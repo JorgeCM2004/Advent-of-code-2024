535a97bc720195f7e73e25689f925ce17dfca5e4
JorgeCM <harryj3699@gmail.com>
2024-12-06 17:06:22 +0100

    D1_P1

---
+++ Day_1/Problem_1/D1_P1.py
@@ -0,0 +1,19 @@
+import os
+
+DIRECTORY_PATH = os.path.abspath(os.path.join(__file__, "..", ".."))
+FILE_NAME = "input.txt"
+
+with open(DIRECTORY_PATH + "\\" + FILE_NAME, "r") as file:
+    list_group1: list[int] = []
+    list_group2: list[int] = []
+    for line in file:
+        value1, value2 = map(int, line.strip().split())
+        list_group1.append(value1)
+        list_group2.append(value2)
+
+list_group1.sort()
+list_group2.sort()
+total = 0
+for value1, value2 in zip(list_group1, list_group2):
+	total += abs(value1 - value2)
+print(f"Total distance: {total}")
